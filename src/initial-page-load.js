84a5c34c545f307e956c7f6d11e6c02ec0e69a0c
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-10-22 19:40:36 +0300

    a picture imported and appended to "content"

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,5 +1,12 @@
+import Picture1 from './src/anna-pelzer-IGfIGP5ONV0-unsplash.jpg' 
+
 function initialPageLoad() {
     const contentElement = document.getElementById("content");
+
+    // Photo by <a href="https://unsplash.com/@annapelzer?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Anna Pelzer</a> on <a href="https://unsplash.com/photos/IGfIGP5ONV0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+    const thePicture = new Image();
+    thePicture.src = Picture1;
+    contentElement.appendChild(thePicture);
 
     return contentElement;
 }
