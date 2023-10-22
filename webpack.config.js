1d55aed895ca972ace29229645c823717c481218
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-10-23 00:38:11 +0300

    output name changed; clean option erased

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
     module: {
         rules: [
