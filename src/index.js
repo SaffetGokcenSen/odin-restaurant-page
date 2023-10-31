a536508997f1dc4b27f05e89e7cd20da34bb675f
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-11-01 00:56:58 +0300

    tab buttons added to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import {initialPageLoad} from "./initial-page-load.js";
 
-initialPageLoad();
+initialPageLoad(); 
+
+const contentElement = document.getElementById("content"); 
+const theContactTab = document.createElement("button"); 
+theContactTab.textContent = "CONTACT";
+const theHomeTab = document.createElement("button");
+theHomeTab.textContent = "HOME";
+const theMenuTab = document.createElement("button");
+theMenuTab.textContent = "MENU"; 
+contentElement.appendChild(theHomeTab);
+contentElement.appendChild(theMenuTab);
+contentElement.appendChild(theContactTab);
