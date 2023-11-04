7a18895398955981970f278b36ba01c589fed5d9
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-11-04 20:09:38 +0300

    comments added

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -1,14 +1,22 @@
-import './styles.css'
+import './styles.css' // the styling file is imported
 
+// this function makes the initial page load
 function initialPageLoad() {
+    // the element with the content of the webpage is accessed
     const contentElement = document.getElementById("content");
+    // the div where the welcome header is written is created
     const welcomeDiv = document.createElement("div"); 
+    // the class of the welcome div is set
     welcomeDiv.className = "welcomeDiv";
+    // the welcome header is created
     const welcomeHeader = document.createElement("h1");
+    // the welcome message is defined
     welcomeHeader.textContent = "WELCOME TO THIS GREAT RESTAURANT";
+    // the welcome message is added to the welcome div 
     welcomeDiv.appendChild(welcomeHeader);
+    // the welcome div is added to the content
     contentElement.appendChild(welcomeDiv);
-
 }
 
+// initialPageLoad function is exported to be used
 export {initialPageLoad};
