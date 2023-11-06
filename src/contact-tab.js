9495e880f9b6b998bf9e87200f56363aa0600211
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-11-07 00:43:36 +0300

    tabContent is made a flex container

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -1,6 +1,12 @@
 // the function to implement the contact tab
 function contactTab() {
-    
+    // access to the content element
+    const contentElement = document.getElementById("content"); 
+    // access to the tabContent div
+    const tabContentArray = contentElement.getElementsByClassName("tabContent"); 
+    const tabContent = tabContentArray[0];
+    // tabContent is a flex container
+    tabContent.style.display = "flex";
 }
 
 // contactTab function is exported
