6fe2503c94649c0f1afd1a82f0c45155ff50b55a
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-11-26 14:32:51 +0300

    tab content div is styled

--- src/menu-tab.js
+++ src/menu-tab.js
@@ -1,4 +1,21 @@
 // the function to implement the menu tab
 function menuTab() {
-    
-}
+    // access to the content element
+    const contentElement = document.getElementById("content"); 
+    // access to the tabContent div
+    const tabContentArray = contentElement.getElementsByClassName("tabContent"); 
+    const tabContent = tabContentArray[0];
+    // tabContent is a flex container
+    tabContent.style.display = "flex";
+    // the styling for the tabContent
+    tabContent.style.flexDirection = "column";
+    tabContent.style.justifyContent = "center";
+    tabContent.style.alignItems = "center";
+    tabContent.style.gap = "1.0rem";
+    tabContent.style.fontSize = "1.5rem";
+    tabContent.style.color = "yellowgreen";
+    tabContent.style.fontWeight = "bold"; 
+} 
+
+// the menu tab function is exported
+export { menuTab };
